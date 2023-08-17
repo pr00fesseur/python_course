24342317971ecb37a4af5c1473da6c9f16a05b4d
pr00fesseur <qq7337047@gmail.com>
2023-08-18 01:34:09 +0200

    functions homework4

---
+++ lesson5/homework.py
@@ -0,0 +1,41 @@
+team: list[dict] = [
+    {"name": "John", "age": 20, "number": 1},
+    {"name": "Mark", "age": 33, "number": 3},
+    {"name": "Cavin", "age": 17, "number": 12},
+]
+
+
+def show_players(players: list[dict]) -> None:
+    """This function should print all players to the client"""
+    for player in players:
+        print(f"Name: {player['name']}, Age: {player['age']}, Number: {player['number']}")
+
+
+def add_player(num: int, name: str, age: int) -> None:
+    """This function adds the new player."""
+    new_player = {"name": name, "age": age, "number": num}
+    team.append(new_player)
+
+
+def remove_player(players: list[dict], num: int) -> None:
+    """This function removes the player by his/her number."""
+    for player in players:
+        if player["number"] == num:
+            players.remove(player)
+            break
+
+
+def main():
+    show_players(team)
+
+    add_player(num=17, name="Cris", age=31)
+    add_player(num=17, name="Bob", age=39)
+    remove_player(players=team, num=17)
+
+    show_players(team)
+
+
+if __name__ == "__main__":
+    main()
+else:
+    raise SystemExit("This module is only for running")
