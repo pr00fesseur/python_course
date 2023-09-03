dbdd302c6346a44dee691219c8468e1efc7b7942
pr00fesseur <qq7337047@gmail.com>
2023-09-03 22:31:59 +0200

    remaded hw currency

--- lesson9/currency_hw.py
+++ lesson9/currency_hw.py
@@ -1,55 +1,59 @@
 
 
 class Price:
-    exchange_rates = {
-        "USD": {
-            "EUR": 0.93,
-            "RUB": 96.40,
-        },
-        "EUR": {
-            "USD": 1.10,
-            "RUB": 105,
-        },
-        "RUB": {
-            "USD": 0.010,
-            "EUR": 0.0095,
-        },
-    }
+    exchange_rates = {"USD": 1.0,
+                      "EUR": 0.93,
+                      "RUB": 96.40}
+    
 
     def __init__(self, amount: float, currency: str) -> None:
         self.amount: float = amount
         self.currency: str = currency
 
-    def convert_to(self, target_currency: str) -> 'Price':
-        if self.currency == target_currency:
+    def convert_to_usd(self) -> 'Price':
+        if self.currency == "USD":
             return self
+        else:
+            usd_amount = self.amount / Price.exchange_rates[self.currency]
+            return Price(usd_amount, "USD")
 
-        converted_amount = self.amount * Price.exchange_rates[self.currency][target_currency]
-        return Price(converted_amount, target_currency)
+    def convert_from_usd(self, target_currency: str) -> 'Price':
+        if target_currency == "USD":
+            return self
+        else:
+            target_amount = self.amount * Price.exchange_rates[target_currency]
+            return Price(target_amount, target_currency)
 
     def __add__(self, other: 'Price') -> 'Price':
         if self.currency == other.currency:
             new_amount = self.amount + other.amount
             return Price(new_amount, self.currency)
         else:
-            new_amount = self.amount + other.convert_to(self.currency).amount
-            return Price(new_amount, self.currency)
+            usd_self = self.convert_to_usd()
+            usd_other = other.convert_to_usd()
+            total_usd = usd_self.amount + usd_other.amount
+            return Price(total_usd, "USD")
 
     def __sub__(self, other: 'Price') -> 'Price':
         if self.currency == other.currency:
             new_amount = self.amount - other.amount
             return Price(new_amount, self.currency)
         else:
-            new_amount = self.amount - other.convert_to(self.currency).amount
-            return Price(new_amount, self.currency)
+            usd_self = self.convert_to_usd()
+            usd_other = other.convert_to_usd()
+            total_usd = usd_self.amount - usd_other.amount
+            return Price(total_usd, "USD")
 
     def __str__(self) -> str:
         return f"{self.amount:.2f} {self.currency}"
 
 
+
+
+
 # Примеры
-price1 = Price(100, "USD")
-price2 = Price(50, "EUR")
+price1 = Price(300, "USD")
+price2 = Price(70, "RUB")
 
 # Выполнение операций
 result_sum = price1 + price2
