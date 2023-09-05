02e0c671f9c940a277e7550455e38bace586be05
pr00fesseur <qq7337047@gmail.com>
2023-09-05 08:15:38 +0200

    abc with sc and payments

--- lesson10/figures.py
+++ lesson10/figures.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from math import pi
 
-
 # class Shape(ABC):
 #     @abstractmethod
 #     def calculate_area(self) -> float:
--- lesson10/hw_social_medea.py
+++ lesson10/hw_social_medea.py
@@ -1,30 +1,37 @@
-from typing import Union
 from time import time
+from typing import Union
+
 
 class SocialChannel:
     def __init__(self, channel_type: str, followers: int):
         self.channel_type = channel_type
         self.followers = followers
 
+
 class Post:
     def __init__(self, message: str, timestamp: int):
         self.message = message
         self.timestamp = timestamp
 
+
 class YouTube(SocialChannel):
     def post_message(self, message: str):
         print(f"Posting on YouTube: {message}")
+
 
 class Telegram(SocialChannel):
     def post_message(self, message: str):
         print(f"Posting on Telegram: {message}")
 
+
 class Twitter(SocialChannel):
     def post_message(self, message: str):
         print(f"Posting on Twitter: {message}")
 
+
 def post_a_message(channel: SocialChannel, message: str):
     channel.post_message(message)
+
 
 def process_schedule(posts: list[Post], channels: list[SocialChannel]) -> None:
     current_time = time()
@@ -33,7 +40,6 @@ def process_schedule(posts: list[Post], channels: list[SocialChannel]) -> None:
             message = post.message
             for channel in channels:
                 post_a_message(channel, message)
-
 
 
 # Само использованик:
@@ -45,7 +51,7 @@ twitter = Twitter("twitter", 200000)
 channels = [youtube, telegram, twitter]
 
 posts = [
-    Post("Hello, youTube!", 1679817600),  
+    Post("Hello, youTube!", 1679817600),
     Post("Yo, Telegram!", 1679817600),
     Post("Hello, twitter!", 1679817600),
 ]
