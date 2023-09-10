55e89ad3b603237d9a8dda143c5ba1ac72b478c0
pr00fesseur <qq7337047@gmail.com>
2023-09-10 20:41:23 +0200

    abc with sc and payments

--- lesson11/homework.py
+++ lesson11/homework.py
@@ -1,12 +1,15 @@
-import time
-import os
-import requests
-import threading
 import multiprocessing
+import os
+import threading
+import time
+
+import requests
+
 
 def encrypt_file(path: str):
     print(f"Обработка изображения в процессе {os.getpid()}")
     _ = [i for i in range(100_000_000)]
+
 
 def download_image(image_url):
     print(f"Скачивание изображения в потоке {threading.current_thread().name}")
@@ -14,11 +17,16 @@ def download_image(image_url):
     with open("image.jpg", "wb") as f:
         f.write(response.content)
 
+
 if __name__ == "__main__":
     start_time = time.perf_counter()
 
-    encryption_process = multiprocessing.Process(target=encrypt_file, args=("rockyou.txt",))
-    download_thread = threading.Thread(target=download_image, args=("https://picsum.photos/1000/1000",))
+    encryption_process = multiprocessing.Process(
+        target=encrypt_file, args=("rockyou.txt",)
+    )
+    download_thread = threading.Thread(
+        target=download_image, args=("https://picsum.photos/1000/1000",)
+    )
 
     encryption_process.start()
     download_thread.start()
